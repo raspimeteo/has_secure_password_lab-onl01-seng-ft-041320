30649d545aef4c1bbebae3875a2f8b995c15128a
raspimeteo <jjczon@gmail.com>
2020-06-18 10:42:27 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(name: user_params[:name])
-        if @user.authenticate(user_params[:password])
+        if @user && @user.authenticate(user_params[:password])
             session[:user_id] = @user.id
             redirect_to 'welcome'
         else
